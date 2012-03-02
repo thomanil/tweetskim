f3d7c6ac2563a8fa45f35ee1531bb1c6f0e911b9
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2012-03-02 20:24:43 +0100

    Okay maybe fix test before anything else

--- test/test_tweetskim.rb
+++ test/test_tweetskim.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 class TestTweetskim < Test::Unit::TestCase
   
   def test_something
-    assert_equal 2, Tweetskim::First.new.trial
+    assert_equal 4, Tweetskim::First.new.trial
   end
 
 end
